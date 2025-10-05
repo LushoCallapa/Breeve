bf1921f9001fd41a4e252aea496de3607c4c4b28
LushoCallapa <luiscallapa2004@gmail.com>
2025-10-05 09:16:47 -0400

    MVP

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,9 @@ import Home from './pages/Home'
 import Layout from './components/Layout'
 import Information from './pages/Information'
 import AirQualityMap from './pages/Map'
-import WeeklyForecast from './pages/Data'
+import Data from './pages/Data'
 
 function App() {
-  const weeklyForecastData = [
-    { day: 'Hoy', value: 191, color: 'bg-red-400' },
-    { day: 'Lunes', value: 180, color: 'bg-red-400' },
-    { day: 'Martes', value: 174, color: 'bg-red-400' },
-    { day: 'Miércoles', value: 160, color: 'bg-red-400' },
-    { day: 'Jueves', value: 184, color: 'bg-orange-400' },
-    { day: 'Viernes', value: 134, color: 'bg-orange-300' },
-    { day: 'Sábado', value: 125, color: 'bg-yellow-300' },
-  ]; 
 
   return (
     <BrowserRouter>
@@ -23,7 +14,7 @@ function App() {
           <Route index element={<Home />} />
           <Route path="/information" element={<Information />} />
           <Route path="/mapa" element={<AirQualityMap />} />
-          <Route path="/data" element={<WeeklyForecast forecasts={weeklyForecastData}/>}/>
+          <Route path="/data" element={<Data/>}/>
         </Route>
       </Routes>
     </BrowserRouter>
